c26da3bbff5866956789cf48eca12ac0259971b5
julehto <32615813+juleht@users.noreply.github.com>
2025-02-20 18:20:47 +0200

    viela koitetaan

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World! ja Hei Maailma!</h1>')
+  res.send('<h1>Hello World! ja Hei Maailma! ja Viela uusi yritys</h1>')
 })
 
 const PORT = 8080
